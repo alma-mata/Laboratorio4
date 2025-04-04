6ade5b33bc003d6f6bef8012981eb9f2a2a3531f
alma-mata <mat23038@uvg.edu.gt>
2025-04-03 23:11:10 -0600

    Inicio de Laboratorio
    Creación del archivo para el prelaboratorio

---
+++ Laboratorio4/Laboratorio4/main.c
@@ -0,0 +1,29 @@
+/*
+* Laboratorio4.c
+* Created: 3/04/2025 23:01:04
+* Author: Alma Mata
+* Description: 
+*/
+/****************************************/
+// Encabezado (Libraries)
+#include <avr/io.h>
+
+/****************************************/
+// Function prototype
+
+/****************************************/
+// Main Function
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
+/****************************************/
+// NON-Interrupt subroutines
+
+
+/****************************************/
+// Interrupt subroutines
