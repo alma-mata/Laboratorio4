0eb0eec1d90988572044c4b2f6fcbbc9454cf838
alma-mata <mat23038@uvg.edu.gt>
2025-04-06 23:52:31 -0600

    Post Lab
    Se completa el laboratorio 4 exitosamente :)

--- Laboratorio4/Laboratorio4/main.c
+++ Laboratorio4/Laboratorio4/main.c
@@ -2,7 +2,8 @@
 * Laboratorio4.c
 * Created: 3/04/2025 23:01:04
 * Author: Alma Mata
-* Description: 
+* Description: El prelaboratorio es un contador de 8 bits, el laboratorio es un contador que usa un ADC.
+*		El post laboratorio es una alarma que se activa cuando el ADC sobre pasa al contador de 8 bits.
 */
 /****************************************/
 // Encabezado (Libraries)
@@ -15,19 +16,19 @@
 /****************************************/
 // Variables globales
 volatile uint8_t contador = 0;
-uint8_t display_actual = 0;
+uint8_t display_actual = 0b0001;
 uint8_t out_PORTD = 0x40;
 uint8_t valor_ADC = 0;
 uint8_t Tabla7seg[] = {0x40, 0x79, 0x24, 0x30, 0x19, 0x12, 0x02, 0x78, 0x00, 0x10, 0x08, 0x03, 0x46, 0x21, 0x06, 0x0E};
 
 #define DISPLAY1 (1 << PORTB0)	
 #define DISPLAY2 (1 << PORTB1)
-#define MASK_OUTB ((1 << PORTB2) | (1 << PORTB3) | (1 << PORTB4) | (1 << PORTB5))
-#define MASK_OUTC ((1 << PORTC0) | (1 << PORTC1) | (1 << PORTC2) | (1 << PORTC3))
+#define DISPLAY3 (1 << PORTB2)
+#define pin_ALARMA (1 << PORTB3)
 
 // Function prototype
 void setup();
-void update_OUT();
+void comparar_valor();
 
 uint8_t leer_ADC() {
 	ADCSRA |= (1 << ADSC);
@@ -42,21 +43,25 @@ int main(void)
     setup();
     while (1) 
     {
-		update_OUT();
-		
 		// Procesar ADC
 		valor_ADC = leer_ADC();
-		if (display_actual == 0)
+		PORTB = 0x00;
+		if (display_actual == 0b0001)
 		{
-			PORTB = 0x00;
 			PORTD = Tabla7seg[valor_ADC & 0x0F];
 			PORTB |= DISPLAY1;
 		}
-		else{
-			PORTB = 0x00;
+		if (display_actual == 0b0010)
+		{
 			PORTD = Tabla7seg[(valor_ADC & 0xF0)>>4];
 			PORTB |= DISPLAY2;
 		}
+		if (display_actual == 0b0100)
+		{
+			PORTD = contador;
+			PORTB |= DISPLAY3;
+		}
+		comparar_valor();
     }
 	return 0;
 }
@@ -68,7 +73,6 @@ void setup()
 	cli();
 	// Configuración de puertos
 	DDRB = 0xFF;
-	DDRC |= MASK_OUTC;
 	DDRC &= ~((1 << PORTC4) | (1 << PORTC5));
 	PORTC |= (1 << PORTC4) | (1 << PORTC5);
 	DDRD = 0xFF;
@@ -90,11 +94,16 @@ void setup()
 	sei();
 }
 
-void update_OUT()
+void comparar_valor()
 {
-	// Salida del contador en PORTB y PORTC
-	PORTB = (PORTB & ~MASK_OUTB) | ((contador & 0x0F) << 2);
-	PORTC = (PORTC & ~MASK_OUTC) | ((contador >> 4) & 0x0F);
+	if (contador < valor_ADC)
+	{
+		PORTB |= pin_ALARMA;
+	}
+	else
+	{
+		PORTB &= ~pin_ALARMA;
+	}
 }
 
 /****************************************/
@@ -112,5 +121,16 @@ ISR(PCINT1_vect)
 }
 ISR(TIMER0_OVF_vect)
 {	
-	display_actual = !display_actual;
+	if (display_actual == 0b0001)
+	{
+		display_actual = 0b0010;
+	}
+	else{ if (display_actual == 0b0010)
+		{
+			display_actual = 0b0100;
+		}
+		else { 
+				display_actual = 0b0001;
+		}
+	}
 }
