522e8591afb58d4cb5400ea575cd6984e255b5eb
alma-mata <mat23038@uvg.edu.gt>
2025-04-07 16:37:10 -0600

    Arreglos al post laboratorio
    Se agrega una función de filtrado para estabilizar la lectura del ADC

--- Laboratorio4/Laboratorio4/main.c
+++ Laboratorio4/Laboratorio4/main.c
@@ -19,9 +19,10 @@ volatile uint8_t contador = 0;
 uint8_t display_actual = 0b0001;
 uint8_t out_PORTD = 0x40;
 uint8_t valor_ADC = 0;
+uint16_t valor_ADC_filtrado = 0;
 uint8_t Tabla7seg[] = {0x40, 0x79, 0x24, 0x30, 0x19, 0x12, 0x02, 0x78, 0x00, 0x10, 0x08, 0x03, 0x46, 0x21, 0x06, 0x0E};
 
-#define DISPLAY1 (1 << PORTB0)	
+#define DISPLAY1 (1 << PORTB0)
 #define DISPLAY2 (1 << PORTB1)
 #define DISPLAY3 (1 << PORTB2)
 #define pin_ALARMA (1 << PORTB3)
@@ -40,20 +41,24 @@ uint8_t leer_ADC() {
 // Main Function
 int main(void)
 {
-    setup();
-    while (1) 
-    {
+	setup();
+	while (1)
+	{
 		// Procesar ADC
 		valor_ADC = leer_ADC();
+		
+		// Aplicar filtro de media móvil exponencial
+		valor_ADC_filtrado = (valor_ADC_filtrado * 7 + valor_ADC) / 8;
+
 		PORTB = 0x00;
 		if (display_actual == 0b0001)
 		{
-			PORTD = Tabla7seg[valor_ADC & 0x0F];
+			PORTD = Tabla7seg[valor_ADC_filtrado & 0x0F];
 			PORTB |= DISPLAY1;
 		}
 		if (display_actual == 0b0010)
 		{
-			PORTD = Tabla7seg[(valor_ADC & 0xF0)>>4];
+			PORTD = Tabla7seg[(valor_ADC_filtrado & 0xF0)>>4];
 			PORTB |= DISPLAY2;
 		}
 		if (display_actual == 0b0100)
@@ -62,7 +67,7 @@ int main(void)
 			PORTB |= DISPLAY3;
 		}
 		comparar_valor();
-    }
+	}
 	return 0;
 }
 
@@ -71,6 +76,7 @@ int main(void)
 void setup()
 {
 	cli();
+	//SPCR = 0x00; // Asegura que el SPI esté desactivado
 	// Configuración de puertos
 	DDRB = 0xFF;
 	DDRC &= ~((1 << PORTC4) | (1 << PORTC5));
@@ -84,25 +90,21 @@ void setup()
 	// Habilitar interrupciones del TIMER0
 	TCCR0A = 0x00;  // Modo Normal
 	TCCR0B = (1 << CS01) | (1 << CS00);   //Prescaler 64
-	TCNT0 = 131;  
+	TCNT0 = 131;
 	TIMSK0 = (1 << TOIE0);
 	
 	// Configuracion ADC
-	ADMUX = 0;
-	ADMUX |= (1<<REFS0)|(1<<ADLAR)|(1<<MUX2)|(1<<MUX1)|(1<<MUX0);
+	ADMUX = (1 << REFS0) | (1 << ADLAR) | 7; // ADC7 (A7)
 	ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
 	sei();
 }
 
 void comparar_valor()
 {
-	if (contador < valor_ADC)
+	// Solo encender la alarma si el valor filtrado supera el contador con margen
+	if (contador < (valor_ADC_filtrado & 0xFF)) // cast a 8 bits por seguridad
 	{
 		PORTB |= pin_ALARMA;
-	}
-	else
-	{
-		PORTB &= ~pin_ALARMA;
 	}
 }
 
@@ -115,12 +117,12 @@ ISR(PCINT1_vect)
 		contador++;
 	}
 	if(!(PINC & 0b00100000)) // Si es PC5 disminuye contador
-	{ 
+	{
 		contador--;
 	}
 }
 ISR(TIMER0_OVF_vect)
-{	
+{
 	if (display_actual == 0b0001)
 	{
 		display_actual = 0b0010;
@@ -129,8 +131,8 @@ ISR(TIMER0_OVF_vect)
 		{
 			display_actual = 0b0100;
 		}
-		else { 
-				display_actual = 0b0001;
+		else {
+			display_actual = 0b0001;
 		}
 	}
 }
